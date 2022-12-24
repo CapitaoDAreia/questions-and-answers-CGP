707a760d2cd4db100ece0a3c2468b8ea9375cecc
Igor Silva <danieligord7@hotmail.com>
2022-12-24 14:53:42 -0300

    Cleaning directories, installing bootstrap, preparing project.

--- index.ts
+++ index.ts
@@ -3,25 +3,11 @@ import express, { Request, Response } from "express";
 const app = express();
 const PORT = 4000;
 
-//Setting EJS as view engine
 app.set("view engine", "ejs");
+app.use(express.static('public'))
 
 app.get("/", (req: Request, res: Response) => {
-  res.render("index");
-});
-
-app.get("/home", (req: Request, res: Response) => {
   res.render("home");
-});
-
-app.get("/users/:nome/:sobrenome", (req: Request, res: Response) => {
-  const variables = {
-    nome: req.params.nome ?? "sem nome" ,
-    sobrenome: req.params.sobrenome ?? "sem sobrenome",
-    showErrorMessage: true
-  }
-
-  res.render("others/users", {...variables, cargo: "Dev Jr"});
 });
 
 app.listen(PORT, () => {
