0b3e2ada01256829eda572cac4a751ba57a48f4d
Igor Silva <danieligord7@hotmail.com>
2022-12-26 20:46:05 -0300

    Adding action and method to form, sending data to backend and installing bodyParser.

--- index.ts
+++ index.ts
@@ -1,18 +1,28 @@
 import express, { Request, Response } from "express";
+import bodyParser from "body-parser";
 
 const app = express();
 const PORT = 4000;
 
-app.set("view engine", "ejs");
-app.use(express.static('public'))
+app.set("view engine", "ejs"); //set EJS as view engine
+app.use(express.static("public")); //deliver static files
+app.use(bodyParser.urlencoded({ extended: false })); //translates received data in a JS structure
+app.use(bodyParser.json()); //allows to read form data received as JSON
 
 app.get("/", (req: Request, res: Response) => {
   res.render("home");
 });
 
-app.get("/answer", (req: Request, res: Response)=>{
-  res.render("answer")
-})
+app.get("/question", (req: Request, res: Response) => {
+  res.render("question");
+});
+
+app.post("/savequestion", (req: Request, res: Response) => {
+  const title = req.body.questionTitle;
+  const description = req.body.questionDescription;
+
+  res.send(`${title} ${description}`);
+});
 
 app.listen(PORT, () => {
   console.log(`App is running on port ${PORT}`);
